8b831d85b8a5fc913e4df1e95a70ce3840a3177f
Bruno Souza-BRUNOPAT <brunopat@motorola.com>
2023-07-15 00:08:08 -0400

    webapp/ feat: header, footer and login componets creation | config: Angular Material has been installed

--- notax_project/src/main/webapp/src/app/app.module.ts
+++ notax_project/src/main/webapp/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
