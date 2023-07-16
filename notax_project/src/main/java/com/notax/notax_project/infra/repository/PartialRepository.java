ebfe0a2531313124e493f90e18327ac844b62d47
Bruno Souza-BRUNOPAT <brunopat@motorola.com>
2023-07-16 16:35:10 -0400

    review: repositories has been reviewed

--- notax_project/src/main/java/com/notax/notax_project/infra/repository/PartialRepository.java
+++ notax_project/src/main/java/com/notax/notax_project/infra/repository/PartialRepository.java
@@ -3,10 +3,12 @@ package com.notax.notax_project.infra.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.notax.notax_project.infra.entities.DisciplineModel;
 import com.notax.notax_project.infra.entities.PartialModel;
 
+@Repository
 public interface PartialRepository extends JpaRepository<PartialModel, Long> {
     List<PartialModel> findAllByDiscipline(DisciplineModel discipline);
 }
